f831763b20ad7207344535ad4292b67d090eb387
SUFI <safwanm.cse@gmail.com>
2023-02-14 17:26:30 +0600

    comitted

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static ClassLibrary1.Class1;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public class SalaryType
+        {
+            protected void Type(int sal)
+            {
+                if (sal > 10000)
+                    Console.WriteLine("You have a good Salary Range");
+                else
+                    Console.WriteLine("You have a poor Salary Range");
+            }
+        }
+
+        public class Salary : SalaryType
+        {
+                       
+            public void SalaryAcess(int salary)
+            {
+                //passing the value of salary to the parent protected class
+                Type(salary);
+
+            }            
+        }
+
+
+        public abstract class accLevel
+        {
+            //private access modifier used
+            private int limit = 5000;
+
+            public void level(int deposit)
+            {
+                if (deposit >= limit)
+                    Console.WriteLine("Account is premium");
+                else
+                    Console.WriteLine("Account is not premium");
+            }            
+            public abstract void interest(int deposit);
+
+        }
+
+        public class interestAmount : accLevel
+        {
+            public override void interest(int deposit)
+            {
+                //interestPercentage = deposit/10;
+                Console.WriteLine($"10% interest of your deposited amount will be {deposit/10}");
+            }
+        }
+
+        
+
+    }
+}
---
+++ OOP_Assignment/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static ClassLibrary1.Class1;
+
+namespace OOP_Assignment
+{
+    public class Program
+    {
+        static int mainBalance = 1000;
+        //mainBalance is a static integer value
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+       
+            Console.Write("Enter deposit amount: ");
+            int deposit = Convert.ToInt32(Console.ReadLine());
+
+            //object of BankCal class which inherites BlncCal interface
+            BlncCal bankObj = new BankCal();    
+
+            int currentBal;
+            currentBal = bankObj.CurrentBal(name, mainBalance, deposit);
+
+            //creating instances of reference project classes
+            interestAmount interest = new interestAmount();
+            accLevel level = interest;
+
+            //passing value of deposited amount to abstract class and child class
+            level.level(deposit);
+            level.interest(deposit);
+
+            Console.Write("Enter your salary: ");
+            int sal = Convert.ToInt32(Console.ReadLine());
+
+            //passing the value of salary to reference project's class
+            Salary salary = new Salary();
+            salary.SalaryAcess(sal);
+
+            //makes the program wait for a key press
+            Console.ReadKey();
+
+        }
+    }
+
+    public class BankDetails
+    {
+        protected void accName(string name)
+        {
+            Console.WriteLine($"Your account name is {name}");
+        }
+
+        protected void accDeposit(int deposit)
+        {
+            Console.WriteLine($"Your deposited amount is {deposit}");
+        }
+    }
+
+
+    interface BlncCal
+    {
+        int CurrentBal(string accName, int depositAmount, int prevBalance);
+
+    }
+
+    public class BankCal : BlncCal      //interface implementation
+    {
+        int BlncCal.CurrentBal(string accName, int depositAmount, int prevBalance)
+        { 
+            Console.WriteLine($"After depositing, your current balance of {accName} is {depositAmount + prevBalance}");
+            return depositAmount + prevBalance;
+        }
+    }
+}
